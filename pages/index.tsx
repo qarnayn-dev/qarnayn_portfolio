9d85cacd3e0494936b3d136fe573cf779185900c
Qarnayn Khairuddin <qarnaynk@sportivityco.com>
2022-08-31 23:37:28 +0800

    ADD - some illustrations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import SideNavBar from '../components/SideNavBar';
-import DarkThemeToggle from '../components/DarkThemeToggle';
+import DarkThemeToggle, { ThemeContext } from '../components/DarkThemeToggle';
 import ThemeTester from '../components/ThemeTester';
 import HorizontalNav from '../components/HorizontalNav';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Transition } from '@headlessui/react';
 import TopFrame from '../components/TopFrame';
+import DevIllustration from '../assets/sw_dev.svg';
+import DevIllustrationDark from '../assets/sw_dev_dark.svg';
 
 
 const Home: NextPage = () => {
@@ -29,11 +31,16 @@ export default Home
 
 
 const Intro = () => {
+  const {theme} = useContext(ThemeContext)
 
   return (
     <div className={`mt-6 mb-10 w-full float-left relative flex flex-col  justify-start items-center gap-2 mobile-lg:flex-row-reverse`}>
-      <div className='bg-red-100  h-40 min-w-[40%] float-right'></div>
-      <div className='bg-blue-100 h-40 float-left w-full mobile-lg:min-w-[60%]'></div>
+      <div className='min-w-[40%] float-right mx-6 md:mr-10 '>
+        {(theme==='light')? <DevIllustration/>: <DevIllustrationDark/>}
+      </div>
+      <div className='bg-blue-100 float-left w-full'>
+        {/* <h1 className=''>Hi, I'm Qarnayn</h1> */}
+      </div>
     </div>
   )
 }
