bb2fb93248de9064b736891902e32b26a152c2bc
Qarnayn Khairuddin <qarnaynk@sportivityco.com>
2022-09-01 01:22:31 +0800

    UP - responsive improvements

--- components/TopFrame.tsx
+++ components/TopFrame.tsx
@@ -6,10 +6,10 @@ import SideNavBar from './SideNavBar'
 const TopFrame = (props:any) => {
   return (
       <>
-        <div className='mobile-lg:block hidden right-2 mt-4 mr-2 absolute'><DarkThemeToggle /></div>
-        <div className='mobile-lg:block absolute'><SideNavBar /></div>
-        <div className='mobile-lg:block hidden absolute right-0'><HorizontalNav/></div>
-        <div className='mt-10 mobile-lg:mt-24 px-2 mobile-lg:px-4'>
+        <div className='md:block hidden right-2 mt-4 mr-2 absolute'><DarkThemeToggle /></div>
+        <div className='md:hidden absolute'><SideNavBar /></div>
+        <div className='md:block hidden absolute right-0'><HorizontalNav/></div>
+        <div className='mt-10 md:mt-24 px-3 mobile-lg:px-4'>
             {props.children}
         </div>
       </>
