2e02c6af93ec901e7d660834600851f21803fa49
Qarnayn Khairuddin <qarnaynk@sportivityco.com>
2022-10-04 12:57:24 +0800

    UP - revealing page effect improvement

--- components/RevealingPage.tsx
+++ components/RevealingPage.tsx
@@ -6,12 +6,13 @@ import { ParallaxProps } from "react-scroll-parallax/dist/components/Parallax/ty
 interface iRevealingPage{
   children: object[] | object,
   className?: string | undefined,
+  speed?: number | undefined,
   opacityScrolling?: boolean | undefined
 }
 
 /// Storybook like effect i.e. when scrolling down, reveeal  the next page
 /// NOTE: This is the top page layer only, the next page should use <Parallax opacity={[0,2]}>
-export const RevealingPage = ({ children, className, opacityScrolling }: iRevealingPage) => {
+export const RevealingPage = ({ children, className,speed, opacityScrolling }: iRevealingPage) => {
   const [isInview, setIsInView] = useState(false);
   const zPosition: string = isInview ? "z-10" : "z-0";
   const hardCap: number = 0.999;
@@ -19,8 +20,10 @@ export const RevealingPage = ({ children, className, opacityScrolling }: iReveal
 
 
   return (
-    <Parallax
-        opacity={opacityScrolling ?[0,2]:undefined}
+    <>
+      <Parallax
+      opacity={opacityScrolling ? [0, 2] : undefined}
+      speed={speed}
       className={`relative w-screen ${className} ${isInview ? "z-10" : "z-0"}`}>
       <>
       {children}
@@ -35,5 +38,7 @@ export const RevealingPage = ({ children, className, opacityScrolling }: iReveal
         onExit={()=>{if(isInview)setIsInView(false)}}
         className={`w-full absolute bottom-0 right-0 h-20 bg-gradient-to-t from-transparent  ${isInview?'shadow-end-page':'shadow-none'}`}></Parallax>
     </Parallax>
+    <Parallax opacity={[1,-1]} className="w-screen h-screen bg-themed-gray-base z-10 absolute top-[100%] shadow-lg backdrop-blur-lg "></Parallax>
+    </>
   )
 }
