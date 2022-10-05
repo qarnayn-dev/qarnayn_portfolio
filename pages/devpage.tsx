3affc45f997887f0b616e4e442e1abe43b9e0be5
Qarnayn Khairuddin <qarnaynk@sportivityco.com>
2022-10-05 10:30:46 +0800

    DEV - animated sentence sample

--- pages/devpage.tsx
+++ pages/devpage.tsx
@@ -1,38 +1,26 @@
 import React from 'react'
-import {RevealingPage} from '../components/RevealingPage'
 import TopFrame from '../components/TopFrame'
+import { AnimatedColoredText } from '../components/AnimatedColoredText'
+import { AnimatedSentences } from '../components/AnimatedSentences'
 
 
 const Devpage = () => {
+  const fullScreenConfig: string = "w-screen h-screen flex flex-col justify-center items-center";
+  const centerChildrenConfig: string = "flex flex-col justify-center items-center";
+  const bgColor: string = 'bg-gradient-to-br from-primary-base to-themed-gray-base';
+
   return (
-    <>
-    <TopFrame/>
-    <ParallaxSample></ParallaxSample>
-    </>
+    <TopFrame>
+      {/* <RevealingPage className={`${fullScreenConfig}  ${centerChildrenConfig} ${bgColor}`}>
+        <div></div>
+      </RevealingPage> */}
+      <div className={`flex flex-col justify-start items-center w-[100vw] h-[400vh] pt-20`}>
+        <AnimatedColoredText isOpen={true}>My journey, I'm sharing with you</AnimatedColoredText>
+
+        <AnimatedSentences title='What is the title?' className='w-[80%] px-4 py-2' isAnimate={true}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa sint dolorum quia delectus eum quisquam reiciendis quasi? Illo recusandae commodi consequuntur et quidem perferendis, dolore, sed saepe adipisci soluta quis.</AnimatedSentences>
+      </div>
+    </TopFrame>
   )
 }
 
 export default Devpage
-
-const ParallaxSample = () => {
-  const fullScreenConfig: string = "relative w-screen h-screen flex flex-col justify-center items-center";
-  return (
-    <>
-    {/* <Parallax  className={` bg-gradient-to-br from-primary-t5 ${fullScreenConfig}`}>
-      <Parallax opacity={[0,1]} className=' w-screen absolute bottom-0 h-40 bg-gradient-to-t from-transparent shadow-end-page'></Parallax>
-    </Parallax> */}
-    <RevealingPage className='bg-gradient-to-br from-primary-t5 w-screen h-screen flex flex-col justify-center items-center'>
-      <div>Heowwow Worldss</div>
-      <div>Heowwow Worldss</div>
-      <div>Heowwow Worldss</div>
-      <div>Heowwow Worldss</div>
-    </RevealingPage>
-    <RevealingPage opacityScrolling={true} className='bg-blue-400 w-screen h-screen flex flex-col justify-center items-center'>
-        <div>sbafvadbafa f aebf a aef</div>
-        <div>sbafvadbafa f aebf a aef</div>
-        <div>sbafvadbafa f aebf a aef</div>
-        <div>sbafvadbafa f aebf a aef</div>
-    </RevealingPage>
-    </>
-  )
-}
