efe3eadbde0c44cb05f6a306b5a744c52fdc475a
Qarnayn Khairuddin <qarnaynk@sportivityco.com>
2022-10-06 14:41:56 +0800

    FIX - animated sentences alignments logic

--- components/AnimatedSentences.tsx
+++ components/AnimatedSentences.tsx
@@ -1,47 +1,42 @@
-import { useState, useEffect, CSSProperties, useRef } from "react";
+import { useState, useEffect } from "react";
 import { easings,useTransition,  animated, useSpring, useSpringRef, useChain } from "react-spring";
 
 interface iAnimatedSentences{
   children: string,
   className?: string,
-  focusConfig?: string,
-  afterTextConfig?: string,
-  textAlignment?: string,
+  twOnAnimateStyle?: string,
+  twOnRestStyle?: string,
+  // either `text-` or `items-` which depends on the positions
+  twTextAlignment?: string,
+  // tailwind `margin` and `padding`
+  twBoundaries?: string,
   title?: string,
-  isAnimate?: boolean,
-  // tailwind margin/ padding for the focused
-  twFocusBoundaries?: string,
+  showAnimation?: boolean,
 }
 
 /**
  * Split and animate the sentence by sentence. Afterward, display all the sentence in one paragraph.
  * It take `children` as the parameter.
- * If you want to ONLY specify the text alignment, use `textAlignment` instead.
- * When the text is in focus, `focusConfig` or the default: `'text-start text-lg mobile-lg:text-xl md:text-2xl font-semibold gray-dark-pallete dark:gray-light-pallete text-themed-gray-t6'` will be applied.
- * When the text is finieshed, `afterTextConfig` or the default: `'text-start text-base mobile-lg:text-lg md:text-xl'` will be applied.
+ * If you want to ONLY specify the text alignment, use `twTextAlignment` instead.
+ * When the text is in focus, `twOnAnimateStyle` or the default: `'text-start text-lg mobile-lg:text-xl md:text-2xl font-semibold gray-dark-pallete dark:gray-light-pallete text-themed-gray-t6'` will be applied.
+ * When the text is finieshed, `twOnRestStyle` or the default: `'text-start text-base mobile-lg:text-lg md:text-xl'` will be applied.
  */
-export const AnimatedSentences = ({ children,className, focusConfig, afterTextConfig,textAlignment, title,isAnimate:showAnimation = false, twFocusBoundaries}: iAnimatedSentences) => {
+export const AnimatedSentences = ({ children,className, twOnAnimateStyle,  twOnRestStyle,twTextAlignment,twBoundaries, title,showAnimation = false}: iAnimatedSentences) => {
   const fractions: string[] = children.match(/([^\.!\?]+[\.!\?]+)|([^\.!\?]+$)/g)?.filter((value) => (value !== ' ' && value !== undefined) ? value : null).map((value) => value.trim()) ?? [];
   if (title) fractions.unshift(title);
 
-  // const [showAnimation, setShowAnimation] = useState(isAnimate);
-  const [isFinished, setIsFinished] = useState(false);
   const [current, setCurrent] = useState(fractions[0]);
+  const [afterEffect, setAfterEffect] = useState(false);
 
-  const config: string = "flex flex-col items-center justify-center relative";
-  const defaultFocusConfig: string = `${textAlignment??'text-start'} text-lg mobile-lg:text-xl md:text-2xl font-medium gray-dark-pallete dark:gray-light-pallete text-themed-gray-t6`;
-  const defaultTextAfterConfig: string = `${textAlignment??'text-start'} style-body`;
+  const config: string = "flex flex-col justify-center";
+  const onAnimateStyle: string = twOnAnimateStyle ?? `${twTextAlignment??'items-start'} text-lg mobile-lg:text-xl md:text-2xl font-medium gray-dark-pallete dark:gray-light-pallete text-themed-gray-t6`;
+  const onRestStyle: string = twOnRestStyle ?? `${twTextAlignment ?? 'text-start'} style-body`;
+  const defaultSize: string = "w-screen h-[10%]";
+  const boundaries: string = twBoundaries ?? "px-6 mobile-lg:px-8 md:px-12 lg:px-16";
 
-  const titleSpringRef = useSpringRef();
-  const titleSpring = useSpring({
-    ref: titleSpringRef,
-    to: { opacity: isFinished? 1 : 0},
-    config: { duration: 1400, easing: easings.easeInCirc},
-  });
-
-  const transRef = useSpringRef();
+  // const transRef = useSpringRef();
   const transition = useTransition(showAnimation? current:'', {
-    ref: transRef,
+    // ref: transRef,
     from: { opacity: 0},
     enter: { opacity: 1},
     leave: { opacity: 0},
@@ -50,10 +45,17 @@ export const AnimatedSentences = ({ children,className, focusConfig, afterTextCo
     trail : 600,
   });
 
+  const titleSpringRef = useSpringRef();
+  const titleSpring = useSpring({
+    ref: titleSpringRef,
+    to: { opacity: afterEffect? 1 : 0},
+    config: { duration: 1400, easing: easings.easeInCirc},
+  });
+
   const afterEffectSpringRef = useSpringRef();
   const afterEffectSpring = useSpring({
     ref: afterEffectSpringRef,
-    to: { opacity: isFinished? 1 : 0, x:isFinished? 0 : 100},
+    to: { opacity: afterEffect? 1 : 0, x:afterEffect? 0 : 100},
     config: { duration: 800, easing: easings.easeOutCirc},
   });
 
@@ -65,39 +67,37 @@ export const AnimatedSentences = ({ children,className, focusConfig, afterTextCo
       const timeFactor: number = 70;
       // set interval for every dynamically
       const timesUp: number = (current.length < 42)? 3000 : (current.length) * timeFactor;
-      if (!isFinished && currentIndex != -1 && currentIndex < fractions.length - 1) {
+      if (showAnimation && currentIndex != -1 && currentIndex < fractions.length - 1) {
         const timeOut = setTimeout(() => setCurrent(fractions[currentIndex + 1]), timesUp);
         return () => clearTimeout(timeOut);
       }
-      else if (!isFinished && currentIndex === fractions.length - 1) {
+      else if (showAnimation && currentIndex === fractions.length - 1) {
         const timeOut = setTimeout(() => {
           setCurrent('');
-          setIsFinished(true);
+          setAfterEffect(true);
         }, timesUp);
         return () => clearTimeout(timeOut);
       }
     }
-    console.log("showAnimation :",showAnimation);
   }, [showAnimation,current]);
 
-  useChain([transRef,titleSpringRef,afterEffectSpringRef],[0,0.2,0.4]);
+  useChain([titleSpringRef, afterEffectSpringRef],[0,0.4]);
+
+  function onFocusAnimation() {
+    return transition(({ opacity }, item) => (
+      <animated.div
+        style={{opacity:opacity}}
+        className={`${className ?? defaultSize} ${boundaries} ${config} ${onAnimateStyle} w-full absolute`}> {item}</animated.div>
+    ));
+  }
 
   return (
-    <div className={`${className ?? 'w-screen h-[10%]'} ${config} ${afterTextConfig ?? defaultTextAfterConfig} `}>
-      <animated.div style={titleSpring} className="style-heading font-medium mb-4 w-full">{title}</animated.div>
-      {
-        (isFinished) ?
-          <animated.div style={afterEffectSpring}>{children}</animated.div> :
-          transition(({ opacity}, item, _, index) => (
-            <animated.div
-                key={`acacs#${index}`}
-                style={{opacity: opacity}}
-                className={`${focusConfig ?? defaultFocusConfig} ${twFocusBoundaries} absolute w-full`}
-                >
-                {item}
-            </animated.div>
-          ))
-      }
-    </div>
+    <>
+      <div className={`${className ?? defaultSize} ${boundaries} ${config} ${onRestStyle} relative items-center bg-green-200`}>
+        <animated.div style={titleSpring} className="style-heading font-medium mb-4 w-full">{title}</animated.div>
+        <animated.div style={afterEffectSpring}>{children}</animated.div>
+        {onFocusAnimation()}
+      </div>
+    </>
   )
  }
--- pages/devpage.tsx
+++ pages/devpage.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TopFrame from '../components/TopFrame'
 import { AnimatedColoredText } from '../components/AnimatedColoredText'
 import { AnimatedSentences } from '../components/AnimatedSentences'
+import { DummyBlock } from '../components/DummyBlock'
 
 
 const Devpage = () => {
   const fullScreenConfig: string = "w-screen h-screen flex flex-col justify-center items-center";
   const centerChildrenConfig: string = "flex flex-col justify-center items-center";
   const bgColor: string = 'bg-gradient-to-br from-primary-base to-themed-gray-base';
+  const [isShow, setIsShow] = useState(false);
+
+  useEffect(() => {
+    console.log("show?: ", isShow);
+  }, [isShow]);
 
   return (
     <TopFrame>
@@ -16,8 +22,14 @@ const Devpage = () => {
       </RevealingPage> */}
       <div className={`flex flex-col justify-start items-center w-[100vw] h-[400vh] pt-20`}>
         <AnimatedColoredText isOpen={true}>My journey, I'm sharing with you</AnimatedColoredText>
-
-        <AnimatedSentences title='What is the title?' className='w-[80%] px-4 py-2' isAnimate={true}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa sint dolorum quia delectus eum quisquam reiciendis quasi? Illo recusandae commodi consequuntur et quidem perferendis, dolore, sed saepe adipisci soluta quis.</AnimatedSentences>
+        <button
+          onClick={()=>setIsShow((state)=> !state)}
+          className='m-10 hover:scale-125 transition-all duration-700 ease-out'><DummyBlock size='lg' colorChoose={18}></DummyBlock></button>
+        <AnimatedSentences
+          title='What is the title?'
+          showAnimation={isShow}
+          twTextAlignment="text-end"
+        >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa sint dolorum quia delectus eum quisquam reiciendis quasi? Illo recusandae commodi consequuntur et quidem perferendis, dolore, sed saepe adipisci soluta quis.</AnimatedSentences>
       </div>
     </TopFrame>
   )
