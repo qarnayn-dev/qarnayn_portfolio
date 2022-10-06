ec5d5d845e62c98cd5e93f4c3b0459aae9ac0a6c
Qarnayn Khairuddin <qarnaynk@sportivityco.com>
2022-10-06 17:27:54 +0800

    ADD - utilities for reading window dimension

---
+++ components/useWindowDimensions.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+
+
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState({ width: 0, height: 0 });
+
+useEffect(() => {
+    // first initialisation
+    handleResize();
+
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    function getWindowDimensions() {
+        const { innerWidth: width, innerHeight: height } = window;
+        return {width, height};
+    }
+
+    window.addEventListener('resize', ()=> setWindowDimensions(getWindowDimensions()));
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+    // useEffect(() => {
+    //     console.log("width, height : ",`${windowDimensions.width},${windowDimensions.height}`);
+    // },[windowDimensions])
+
+  return windowDimensions;
+}
