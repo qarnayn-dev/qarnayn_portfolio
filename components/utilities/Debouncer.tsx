cef034e5a4107e63f27decfb692b8b0e15efbcdc
Qarnayn Khairuddin <qarnaynk@sportivityco.com>
2022-10-15 21:37:12 +0800

    ADD - debouncer class utility

---
+++ components/utilities/Debouncer.tsx
@@ -0,0 +1,28 @@
+/**
+ * Execute the `callback` when the time (in ms) `executeIn` has completed.
+ * Whenever there's a new `rebound()` call, the timeout will be refreshed.
+ * ```
+ * const debouncer = new Debouncer(()=>{console.log("execute this")}, 1000);
+ * // whenever to be refreshed
+ * debouncer.rebound();
+ * // to cancle:
+ * debouncer.cancel();
+ * ```
+ */
+export class Debouncer {
+    private timeout?: NodeJS.Timeout;
+    constructor(private callback: ()=> void,private executeIn: number) {
+        this.executeIn = executeIn;
+        this.callback = callback;
+    }
+
+    rebound(): void {
+        console.log("calls rebound");
+        if (this.timeout) clearTimeout(this.timeout);
+        this.timeout = setTimeout(this.callback, this.executeIn);
+    }
+
+    cancel(): void{
+        clearTimeout(this.timeout);
+    }
+}
