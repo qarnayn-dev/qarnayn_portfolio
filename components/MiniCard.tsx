56e85cb1f50f628ce1deee1ba2bdaabe429f3884
Qarnayn Khairuddin <qarnaynk@sportivityco.com>
2022-10-25 12:26:01 +0800

    UP - animation on the minicard

--- components/MiniCard.tsx
+++ components/MiniCard.tsx
@@ -1,4 +1,6 @@
+import { useInView} from "framer-motion";
 import Lottie from "lottie-react"
+import { useRef} from "react"
 
 interface iMiniCard{
   animationData: unknown,
@@ -7,15 +9,23 @@ interface iMiniCard{
 }
 
 export const MiniCard = ({ animationData, children }: iMiniCard) => {
+    const titleRef = useRef(null);
+    const isInView = useInView(titleRef, { once: true });
 
-
-  return (
-    <div className='h-full w-full float-left rounded-lg bg-themed-gray-base bg-opacity-10 flex flex-col items-center justify-center text-center px-[7vw] lg:px-[2vw] py-8 apply-glass'>
-      <Lottie animationData={animationData} className='my-6 h-24 mobile-lg:h-28'></Lottie>
-      <div className='h-28 lg:h-36 w-full font-normal style-subheading'>{children}</div>
-      <div className='pt-4 mobile-lg:pt-2 text-primary-t2 font-medium cursor-pointer hover:text-primary-base hover:scale-110 transition-all duration-500 ease-in-out'>
-        see on how i did →
-      </div>
-    </div>
-  )
+    return (
+        <div className='h-full w-full float-left rounded-lg bg-themed-gray-base bg-opacity-10 flex flex-col items-center justify-center text-center px-[7vw] lg:px-[2vw] py-8 apply-glass'>
+        <Lottie animationData={animationData} className='my-6 h-24 mobile-lg:h-28'></Lottie>
+        <div
+            ref={titleRef}
+            className='h-28 lg:h-36 w-full font-normal style-subheading'
+            style={{
+                transform: isInView ? "none" : "translateY(200px)",
+                opacity: isInView ? 1 : 0,
+                transition: "all 0.7s cubic-bezier(.41,.06,.7,1.29) 0.4s"}}>
+            {children}</div>
+        <div className='pt-4 mobile-lg:pt-2 text-primary-t2 font-medium cursor-pointer hover:text-primary-base hover:scale-110 transition-all duration-500 ease-in-out'>
+            see on how i did →
+        </div>
+        </div>
+    )
  }
