8d31b4d435d333366f99c66b8f4797b8392b7a1b
Qarnayn Khairuddin <qarnaynk@sportivityco.com>
2022-10-27 11:48:58 +0800

    FIX - popup modal exceed the screen viewport

--- components/PopupModal.tsx
+++ components/PopupModal.tsx
@@ -1,7 +1,9 @@
 import { Transition } from "@headlessui/react";
 import { useScroll } from "framer-motion";
+import { useEffect, useRef } from "react";
 import { IoClose } from "react-icons/io5";
 import OutsideAlerter from "./useOutsideAlerter";
+import useWindowDimensions from "./useWindowDimensions";
 
 interface iPopupModal{
     children?:any,
@@ -12,21 +14,27 @@ interface iPopupModal{
     closeIcon?: boolean,
 }
 
-export default function PopupModal({ children, isOpen = true, onClose, barrierDismissable = true, className,closeIcon = true}: iPopupModal) {
-    const { scrollY } = useScroll();
+export default function PopupModal({ children, isOpen = true, onClose, barrierDismissable = true, className, closeIcon = true }: iPopupModal) {
+    const ref = useRef<HTMLDivElement>(null);
+    const { scrollY } = useScroll({ target: ref });
+    const {height} = useWindowDimensions();
 
+    function getScrollY(): number{
+        return scrollY.get() - (height*0.1)
+    }
 
     return (
         <Transition
+            ref={ref}
             show={isOpen}
-            style={{ top: scrollY.get() }}
+            style={{ top: getScrollY() }}
             enter="transition-all duration-300"
             enterFrom="opacity-0"
             enterTo="opacity-100"
             leave="transition-all duration-700"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
-            className="absolute h-screen w-screen z-50 inset-0 bg-black bg-opacity-20 dark:bg-opacity-30 backdrop-blur-md flex flex-col items-center justify-center">
+            className="absolute h-[110vh] w-screen z-50 inset-0 bg-black bg-opacity-20 dark:bg-opacity-30 backdrop-blur-md flex flex-col items-center justify-center">
             <OutsideAlerter onOutsideClick={(barrierDismissable)?()=> onClose():undefined}>
                 <div className={`relative ${className ?? 'w-[80vw] h-[80vh] 2xl:w-[1200px] py-4 md:py-5 xl:py-6 px-[3vw] bg-themed-gray-t2 rounded-lg mobile-lg:rounded-xl'}`}>
                     {closeIcon && <IoClose  onClick={()=>onClose()} className="absolute top-0 right-0 mr-2 mt-2 text-themed-gray-inverse text-opacity-50 style-subheading"/>}
