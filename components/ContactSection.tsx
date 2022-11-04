a55c7b19556a113d70853a513bd133e8276ab3a5
Qarnayn Khairuddin <qarnaynk@sportivityco.com>
2022-11-04 17:10:02 +0800

    ADD - layout for the contact form

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import React, { ChangeEvent, useEffect, useMemo, useReducer, useState } from 'react'
+import { TextInputField } from './TextInputField';
 
 export const ContactSection = () => {
   return (
@@ -23,63 +24,31 @@ const ContactMeForm = () => {
     }
 
     return (
-        <div className='w-full max-w-lg h-[80%] rounded-xl px-4 py-8 bg-themed-gray-base drop-shadow-md dark:bg-neutral-900 style-body'>
+        <div className='w-full max-w-lg rounded-xl px-4 mobile-lg:px-6 sm:px-8 pt-16 pb-6 bg-themed-gray-base drop-shadow-md dark:bg-neutral-900 style-body flex flex-col justify-center items-start'>
             <TextInputField title='Name' inputKeyName="name" value={state.name} onChange={handleInputChange} />
             <TextInputField title='Title' inputKeyName="title" value={state.title} onChange={handleInputChange} />
             <TextInputField title='Email' inputKeyName="email" value={state.email} onChange={handleInputChange} />
-            <TextInputField title='Message' hintText="Write me anything. It's that easy to get in touch!" inputKeyName="message" value={state.message} onChange={handleInputChange} useTextArea/>
-            <button className='bg-red-400 px-4 py-2' onClick={()=>checkForState()}>test</button>
+            <TextInputField title='Message' hintText="Write me anything. It's that easy to get in touch!" inputKeyName="message" value={state.message} onChange={handleInputChange} useTextArea />
+            <div className='w-full  mt-4 mb-2relative'>
+                <div className='style-small-text text-primary-t4 '>optional</div>
+                <div className='mb-3'>Scouting for talent? What role are you interested in?</div>
+                <div className='w-full gap-2 flex flex-wrap'>
+                    <SelectableTag>UI/UX Designer</SelectableTag>
+                    <SelectableTag>Front-end dev</SelectableTag>
+                    <SelectableTag>Back-end dev</SelectableTag>
+                    <SelectableTag>Product engineer</SelectableTag>
+                    <SelectableTag>Software engineer</SelectableTag>
+                    <SelectableTag>Data engineer</SelectableTag>
+                </div>
+            </div>
+            <button className='mt-16 mb-2 px-2 py-1 bg-primary-t2 text-themed-gray-base dark:text-themed-gray-inverse style-body shadow-sm dark:shadow-themed-gray-t3 font-normal rounded-md ' onClick={()=>checkForState()}>Send Message</button>
         </div>
     )
 }
 
-interface iTextInputField{
-    inputKeyName: string,
-    title: string,
-    value: string,
-    onChange: (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement >) => void,
-    hintText?: string,
-    labelId?: string,
-    useTextArea?: boolean,
-    className?: string,
-}
-
-const TextInputField = (props:iTextInputField) => {
-    const [isFocus, setIsFocus] = useState(false);
-    const shouldOnTop = useMemo(() => {
-        return (!isFocus && props.value.length === 0)
-    }, [isFocus, props.value.length === 0]);
-
-    const inputBoxCN: string = "w-full px-2 pt-2 pb-1 bg-transparent border-[1px] border-themed-gray-t2 rounded-md outline-none focus:outline-offset-0 focus:duration-500 ease-out-circ focus:outline-primary-t2 focus:border-transparent";
-
+const SelectableTag = (props:any) => {
     return (
-        <div className={`relative group ${props.className ?? 'mb-4'}`}>
-            <motion.label
-                transition={{duration: isFocus ? 0.3 : 0.12}}
-                animate={{
-                    translateY: shouldOnTop ? 0 : -14,
-                    translateX: shouldOnTop ? 6:0,
-                    scale: shouldOnTop ? 0.9 : 0.8,
-                    opacity:  shouldOnTop? 0: 1,
-                }}
-                className='bg-themed-gray-base text-themed-gray-t7 absolute top-0 left-0 px-1 group-focus-within:text-primary-base group-focus-within:font-medium'>{props.title}</motion.label>
-            {!props.useTextArea ?
-            <input
-                name={props.inputKeyName}
-                placeholder={isFocus ? "" : props.hintText ?? props.title}
-                onFocus={() => setIsFocus(true)}
-                onBlur={() => setIsFocus(false)}
-                onChange={(e) => props.onChange(e)}
-                className={`${inputBoxCN}`} />
-            : <textarea
-                name={props.inputKeyName}
-                placeholder={isFocus ? "" : props.hintText ?? props.title}
-                onFocus={() => setIsFocus(true)}
-                onBlur={() => setIsFocus(false)}
-                onChange={(e) => props.onChange(e)}
-                className={`${inputBoxCN} h-20`} />
-            }
-        </div>
+        <button className='px-3 py-1 bg-themed-gray-t3 rounded-2xl overflow-clip style-small-text text-themed-gray-t9'>{props.children}</button>
     )
  }
 
