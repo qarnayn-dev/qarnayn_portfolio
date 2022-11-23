62c72f288efe9152f9ece2498ae78b9fe4c7fc17
Qarnayn Khairuddin <qarnaynk@sportivityco.com>
2022-11-23 14:26:39 +0800

    UP - add some UI chaned post

--- components/ChainedPost.tsx
+++ components/ChainedPost.tsx
@@ -1,13 +1,17 @@
 
 interface iChainedPost{
-  children: string[],
-  className?: string,
+    children: string[],
+    header: any,
+    className?: string,
 }
 
 export const ChainedPost = (props: iChainedPost) => {
   return (
-    <div className={`${props.className ?? 'w-full'}`}>
-      {props.children.map((v, i) => <Content key={i} isLastItem={(i === props.children.length - 1)}>{v}</Content> )}
+    <div className={`${props.className ?? 'w-full'} relative`}>
+        <p className="relative mb-9 style-body apply-inverse-gray text-themed-gray-t2 after:absolute after:bottom-0 after:left-0 after:h-8 after:w-2 after:translate-y-9 after:border-l-2 after:border-primary-t4 ">
+            {props.header}
+        </p>
+        {props.children.map((v, i) => <Content key={i} isLastItem={(i === props.children.length - 1)}>{v}</Content> )}
     </div>
   )
 }
@@ -20,15 +24,12 @@ interface iContent{
 
 const Content = ({children,className,isLastItem = false}:iContent) => {
   return (
-    <div className={`relative pl-5 pb-4 mobile-lg:pb-5 xl:pb-6
-      ${className?? 'style-body'}
+    <div className={`relative pl-6 pb-4 mobile-lg:pb-5 xl:pb-6 border-l-2
+      ${className?? 'style-body apply-inverse-gray text-themed-gray-t8'}
       ${isLastItem ? 'border-transparent' : 'border-primary-t3'}
-      border-l-2 mobile-lg:border-l-4
-      before:absolute before:top-0 before:border-primary-t3 before:w-5 before:rounded-bl-full
-      before:h-3 mobile-lg:before:h-4
-      before:-left-[2px] mobile-lg:before:-left-1
-      before:border-b-2 mobile-lg:before:border-b-4
-      before:border-l-2 mobile-lg:before:border-l-4`}>
+      before:absolute before:top-0 before:border-primary-t3 before:rounded-bl-full before:w-5 before:-left-[2px] before:border-b-2 before:border-l-2
+      before:h-3 mobile-lg:before:h-[14px]
+      `}>
       {children}
     </div>
   )
