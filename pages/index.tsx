b6919e83a5e70e42366bcda741608e2530f1a5eb
Qarnayn Khairuddin <qarnaynk@sportivityco.com>
2022-12-01 14:39:12 +0800

    UP - copywriting

--- pages/index.tsx
+++ pages/index.tsx
@@ -196,8 +196,8 @@ const ContactSection = () => {
     <div className='frame-bounded-x pt-32 pb-10 flex flex-col gap-5 md:flex-row bg-primary-base bg-opacity-10 dark:bg-opacity-5 relative'>
       <div className='max-w-[85%] sm:w-full mb-6'>
         <SectionTitle className="mb-6">Contact</SectionTitle>
-        <div className='style-heading mb-8'>Love to hear from you, don’t hesitate to get in touch!</div>
-        <div className='mb-8 style-body max-w-[90%]'>I’m open to any opportunity. Feel free to contact me via the message section or you can send me an email directly. Also, I am on LinkedIn! Come and connect with me in LiknedIn to keep in touch.</div>
+        <div className='style-heading mb-8'>Love to hear from you, send me a message!</div>
+        <div className='mb-8 style-body max-w-[90%]'>I’m open to any opportunity. Feel free to contact me via the message section, or you can send me an email directly. Also, I am on LinkedIn! Come and connect with me on LinkedIn to keep in touch.</div>
         <div className='flex flex-row gap-4'>
           <ClickToCopy textToCopy="qarnaynkhairuddin@gmail.com" message="Qarnayn's email address has been copied to your clipboard.">
             <IconButton icon={IoMailOpen} title="email" tooltip="Copy email address" leftAligned/>
